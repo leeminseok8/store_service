73a3083bbbb1b6cb65e1a9c899a659e24b9d635b
이민석 <dat0802@gmail.com>
2022-09-13 15:10:11 +0900

    (accounts/modeling) 유저 및 관리자 모델 작성

---
+++ accounts/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    """
+    유저 및 관리자를 생성하기 위한 모델
+    회원가입 시 필수 입력 사항 : [username, mobile, address, password]
+    """
+
+    def create_user(self, username, mobile, address, password=None):
+        if not username:
+            raise ValueError("계정 이름을 입력해주세요.")
+        if not password:
+            raise ValueError("비밀번호를 입력해주세요.")
+        if not mobile:
+            raise ValueError("핸드폰 번호을 입력해주세요.")
+        if not address:
+            raise ValueError("주소를 입력해주세요.")
+
+        user = self.model(
+            username=username,
+            password=password,
+            mobile=mobile,
+            address=address,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, mobile, address, password=None):
+        user = self.create_user(
+            username=username,
+            password=password,
+            mobile=mobile,
+            address=address,
+        )
+
+        user.is_staff = True
+
+        user.save(using=self._db)
+        return user
+
+
+class Account(AbstractBaseUser):
+    name = models.CharField(verbose_name="이름", max_length=10)
+    username = models.CharField(verbose_name="아이디", max_length=20)
+    password = models.CharField(verbose_name="비밀번호", max_length=128)
+    sex = models.BooleanField(verbose_name="성별", default=True)
+    mobile = models.CharField(verbose_name="핸대폰 번호", max_length=15)
+    address = models.CharField(verbose_name="주소", max_length=100)
+
+    # state
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["mobile", "address"]
+
+    class meta:
+        db_table = "accounts"
