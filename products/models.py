7a6a49e8b76248e39d2f8587f8231f777e7089bb
이민석 <dat0802@gmail.com>
2022-09-14 22:38:09 +0900

    (products/modeling) 상품 모델 및 마이그레이션 파일 수정

--- products/models.py
+++ products/models.py
@@ -1,18 +1,31 @@
 from django.db import models
 
+from accounts.models import User
 from utils.timestamp import TimeStampedModel
 
 
 class Product(TimeStampedModel):
-    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
     content = models.TextField()
     price = models.IntegerField()
     origin = models.CharField(max_length=100)
+    # user = models.ManyToManyField(User, through="UserProduct")
 
     class Meta:
         db_table = "products"
 
 
+# class UserProduct(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+#     class Meta:
+#         db_table = "users_products"
+
+
 class Thumbnail(TimeStampedModel):
     image = models.CharField(max_length=1000)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "thumbnails"
