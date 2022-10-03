d7cf904024570a8f5f68298b13ee5fbab9be7397
이민석 <dat0802@gmail.com>
2022-10-03 23:09:35 +0900

    Feat: 결제 완료 및 취소 뷰, 시리얼라이저 생성

---
+++ payments/serializers.py
@@ -0,0 +1,32 @@
+# import uuid
+
+from rest_framework import serializers
+from orders.models import Order
+
+from payments.models import Payment
+
+
+class PaymentCreateSerializer(serializers.ModelSerializer):
+    """
+    결제 생성 시리얼라이저
+    """
+
+    class Meta:
+        model = Payment
+        fields = ["payment_method", "payment_state", "order"]
+
+
+class PaymentDeleteSerializer(serializers.ModelSerializer):
+    """
+    결제 취소 시리얼라이저
+    """
+
+    class Meta:
+        model = Payment
+        fields = ["payment_state"]
+
+    def update(self, instance, validated_data):
+        instance.payment_state = False
+        instance.save()
+
+        return instance
